06795b40b51dfdd315dfa317527553e83a1dc72b
clairechein0627 <clairechien0627@gmail.com>
2024-11-30 20:19:27 +0800

    結合brick和breakout

--- Test/_Breakout.asm
+++ Test/_Breakout.asm
@@ -36,6 +36,21 @@ ENDM
 	planChar BYTE '='
 	ballChar BYTE '@'
 	wallChar BYTE '|'
+	clearChar BYTE ' '
+	maxX EQU 79
+	maxY EQU 24
+	brickTypeNum EQU 2
+	brickminX EQU 1
+	brickminY EQU 1
+	brickmaxX EQU 79
+	brickmaxY EQU 22
+	brickX byte ?
+	brickY byte ?
+	brick DWORD 22 DUP(79 DUP(0))
+	brickChar byte ' ','-'
+	brickFallTime BYTE 10
+	countTime BYTE 0
+	endGame BYTE 0
 .code 
 
 BreakOut PROC 
@@ -45,24 +60,50 @@ BreakOut PROC
 
 	setGame:
 	CALL setPlan
+	call newBrick
+	call drawBrick
 	
 	goGame:
 	CALL movePlan
 	CALL drawPlan
+	CALL clearBall
+	CALL movBall
 	CALL drawBall
 	CALL drawWall
-	CALL movBall
 	CALL setBallDir
-	INVOKE Sleep, 80
+	call checkBrick
+	CALL checkBall
+	cmp endGame, 1
+	je endBrickGame
+
+	mov al, countTime
+	inc al
+	cmp al, brickFallTime
+	jne noBrickfall
+
+	brickFall:
+	call fall
+	call newBrick
+	call drawBrick
+	mov al, 0
+
+	noBrickFall:
+	mov countTime, al
+
+	INVOKE Sleep, 100
 	mov al, ballY
 	cmp al, maxY
-	jne goGame
 
+	cmp endGame, 1
+	je endBrickGame
+	jmp goGame
 
+	endBrickGame:
 	CALL WaitMsg
 
 	INVOKE	ExitProcess, 0
 	ret
+
 BreakOut ENDP 
 
 
@@ -76,7 +117,6 @@ DrawTitle PROC
 	ret
 
 DrawTitle ENDP
-
 
 setPlan PROC
 	mov al, minX
@@ -150,6 +190,18 @@ movePlan PROC
 	MoveLeft:
         mov ecx, planLength
         mov edi, esi
+
+		mov bl, [esi]
+		dec bl
+		mGoto bl, planY
+		mov al, planChar
+		CALL WriteChar
+
+		mov bl, [esi + planLength -1]
+		mGoto bl, planY
+		mov al, clearChar
+		CALL WriteChar
+
     MoveLeftLoop:
         dec BYTE PTR [edi]
         inc edi
@@ -171,6 +223,17 @@ movePlan PROC
 	MoveRight:
         mov ecx, planLength
         mov edi, esi
+
+		mov bl, [esi]
+		mGoto bl, planY
+		mov al, clearChar
+		CALL WriteChar
+
+		mov bl, [esi + planLength -1]
+		inc bl
+		mGoto bl, planY
+		mov al, clearChar
+		CALL WriteChar
     MoveRightLoop:
         inc BYTE PTR [edi]
         inc edi
@@ -183,7 +246,7 @@ movePlan ENDP
 
 
 drawPlan PROC
-	CALL ClrScr
+
 	mov esi, OFFSET planX
 	mov ecx, planLength
 
@@ -205,6 +268,14 @@ drawBall PROC
 
 	ret
 drawBall ENDP
+
+clearBall PROC
+	mGoTo ballX, ballY
+	mov al, clearChar
+	call WriteChar
+
+	ret
+clearBall ENDP
 
 
 movBall PROC
@@ -275,14 +346,27 @@ setBallDir PROC
 
 setBallDir ENDP
 
+checkBall PROC
+	mov al, ballY
+	cmp al, maxY
+	jne endCheckBall
+	mov endGame, 1
+
+	endCheckBall:
+	ret
+checkBall ENDP
+
 
 drawWall PROC
 
 	LeftWall:
-		mov bl, 0
+		mov bl, minY
+		dec bl
 		mov bh, maxY
 	drawLeftWall:
-		mGoTo 0, bl
+		mov al, minX
+		dec al
+		mGoTo al, bl
 		mov al, wallChar
 		CALL WriteChar
 		inc bl
@@ -290,11 +374,13 @@ drawWall PROC
 		jne drawLeftWall
 
 	RightWall:
-		mov bl, 0
+		mov bl, minY
+		dec bl
 		mov bh, maxY
-		add bh, 2
 	drawRightWall:
-		mGoTo maxX, bl
+		mov al, maxX
+		inc al
+		mGoTo al, bl
 		mov al, wallChar
 		CALL WriteChar
 		inc bl
@@ -303,5 +389,108 @@ drawWall PROC
 
 	ret
 drawWall ENDP
+
+;;
+
+drawBrick proc
+	;call ClrScr
+	mov esi, OFFSET brick
+	mov eax, 0
+	mov ecx, brickmaxY
+Row:
+	push ecx
+	mov ecx, brickmaxX
+Col:
+	xor edx, edx
+	push eax
+	mov ebx, brickmaxX
+	div ebx
+	add dl, 1
+	add al, 1
+	mov brickX, dl
+	mov brickY, al
+	pop eax
+
+	cmp DWORD PTR [esi+eax*4], 1
+	je DrawBrick1
+	jmp DrawBrick0
+
+DrawBrick0:
+	mov dl, brickX
+	mov dh, brickY
+	call Gotoxy
+	push eax
+	mov  eax, red	
+	call SetTextColor
+	mov al, brickChar[0]
+	call Writechar
+	pop eax
+	jmp Continue
+
+DrawBrick1:
+	mov dl, brickX
+	mov dh, brickY
+	call Gotoxy
+	push eax
+	mov  eax, red	
+	call SetTextColor
+	mov al, brickChar[1]
+	call Writechar
+	pop eax
+
+Continue:
+	inc eax
+	loop Col
+	pop ecx
+	loop Row
+	
+ExitPrint:	
+	mov  eax, white	
+	call SetTextColor
+	ret
+drawBrick ENDP
+
+newBrick proc
+	call Randomize
+	mov esi, OFFSET brick
+	mov ecx, brickmaxX
+L:
+	mov eax, brickTypeNum
+	call RandomRange
+	mov [esi], eax
+	add esi, 4
+	loop L
+	ret
+newBrick ENDP
+
+Fall proc
+    mov esi, OFFSET brick + ((brickmaxY-1) * brickmaxX-1) * 4 
+    mov edi, OFFSET brick + (brickmaxY * brickmaxX-1) * 4
+    std                                           
+    mov ecx, (brickmaxY-1)*brickmaxX                               
+    rep movsd                                    
+    cld       
+    ret
+Fall endp
+
+checkBrick proc
+	xor al,al
+	mov esi, OFFSET brick+ (brickmaxY-1) * brickmaxX * 4
+	mov ecx, brickmaxX
+Check:
+	cmp DWORD PTR [esi],1
+	je HaveBrick
+	add esi,4
+	Loop Check
+	jmp Exitcheck
+
+HaveBrick:
+	mov al, 1
+	jmp Exitcheck
+	
+Exitcheck:
+	mov endGame, al
+	ret
+checkBrick endp
 
 END
