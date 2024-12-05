4f3dbd4f75906c27cf90fb88070be9ac2fcf5d8b
77JoyWang77 <wangchaoyin77@gmail.com>
2024-12-05 23:50:19 +0800

    踩地雷

---
+++ Test/_minesweeper.asm
@@ -0,0 +1,166 @@
+.386 
+.model flat,stdcall 
+option casemap:none 
+
+RGB macro red,green,blue
+	xor eax,eax
+	mov ah,blue
+	shl eax,8
+	mov ah,green
+	mov al,red
+endm
+
+include windows.inc 
+include user32.inc 
+include kernel32.inc 
+include gdi32.inc 
+
+WinMain4 proto :DWORD
+check_collision PROTO index:DWORD
+
+.DATA 
+ClassName db "SimpleWinClass", 0 
+AppName  db "Cake", 0 
+ButtonClassName db "button", 0 
+cakeX DWORD 200           ; 初始 X 座標
+cakeY DWORD 80           ; 初始 Y 座標
+cakeWidth DWORD 50       ; 平台寬度
+cakeHeight DWORD 20       ; 平台高度
+stepSize DWORD 50              ; 每次移動的像素數量
+winWidth DWORD 600              ; 視窗寬度
+winHeight DWORD 600             ; 視窗高度
+velocityX DWORD 5               ; 小球 X 方向速度
+velocityY DWORD 0               ; 小球 Y 方向速度
+border_right DWORD 450
+border_left DWORD 150
+maxCakes EQU 10                          ; 最大蛋糕數量
+currentCakeIndex DWORD 0                   ; 當前蛋糕索引
+cakes RECT maxCakes DUP(<0, 0, 0, 0>)      ; 蛋糕陣列，儲存每個蛋糕的邊界
+falling BOOL FALSE                         ; 是否有蛋糕正在掉落
+gameover BOOL TRUE
+fallSpeed DWORD 5                          ; 蛋糕掉落速度
+
+TriesRemaining  db 9
+RemainingTriesText db "Remaining:  ", 0
+EndGame db "Game Over!", 0
+line1Rect RECT <20, 20, 580, 40>
+
+.DATA? 
+hInstance HINSTANCE ? 
+CommandLine LPSTR ? 
+hBrush DWORD ?
+tempWidth DWORD ?
+tempHeight DWORD ?
+
+.CODE 
+Minesweeper PROC 
+start: 
+    invoke GetModuleHandle, NULL 
+    mov    hInstance,eax 
+    invoke GetCommandLine
+    mov CommandLine,eax
+    invoke WinMain4, hInstance
+    ret
+Minesweeper ENDP
+
+WinMain4 proc hInst:HINSTANCE
+    LOCAL wc:WNDCLASSEX 
+    LOCAL msg:MSG 
+    LOCAL hwnd:HWND 
+    LOCAL wr:RECT                   ; 定義 RECT 結構
+
+    ; 定義窗口類別
+    mov   wc.cbSize,SIZEOF WNDCLASSEX 
+    mov   wc.style, CS_HREDRAW or CS_VREDRAW 
+    mov   wc.lpfnWndProc, OFFSET WndProc4
+    mov   wc.cbClsExtra,NULL 
+    mov   wc.cbWndExtra,NULL 
+    push  hInst 
+    pop   wc.hInstance 
+    mov   wc.hbrBackground,COLOR_WINDOW+1 
+    mov   wc.lpszMenuName,NULL 
+    mov   wc.lpszClassName,OFFSET ClassName 
+    invoke LoadIcon,NULL,IDI_APPLICATION 
+    mov   wc.hIcon,eax 
+    mov   wc.hIconSm,eax 
+    invoke LoadCursor,NULL,IDC_ARROW 
+    mov   wc.hCursor,eax 
+    invoke RegisterClassEx, addr wc 
+
+    ; 設置目標客戶區大小
+    mov wr.left, 0
+    mov wr.top, 0
+    mov wr.right, 600 ;改長寬
+    mov wr.bottom, 600
+
+    ; 調整窗口大小
+    invoke AdjustWindowRect, ADDR wr, WS_OVERLAPPED or WS_CAPTION or WS_SYSMENU or WS_MINIMIZEBOX, FALSE
+
+    ; 計算窗口寬度和高度
+    mov eax, wr.right
+    sub eax, wr.left
+    mov tempWidth, eax
+
+    mov eax, wr.bottom
+    sub eax, wr.top
+    mov tempHeight, eax
+
+    ; 創建窗口
+    invoke CreateWindowEx, NULL, ADDR ClassName, ADDR AppName, \
+            WS_OVERLAPPED or WS_CAPTION or WS_SYSMENU or WS_MINIMIZEBOX, \
+            0, 0, tempWidth, tempHeight, NULL, NULL, hInst, NULL
+    mov   hwnd,eax 
+    invoke SetTimer, hwnd, 1, 50, NULL  ; 更新間隔從 50ms 改為 10ms
+    ; 顯示和更新窗口
+    invoke ShowWindow, hwnd,SW_SHOWNORMAL 
+    invoke UpdateWindow, hwnd 
+
+    ; 主消息循環
+    .WHILE TRUE 
+        invoke GetMessage, ADDR msg,NULL,0,0 
+        .BREAK .IF (!eax) 
+        invoke TranslateMessage, ADDR msg 
+        invoke DispatchMessage, ADDR msg 
+    .ENDW 
+    mov     eax,msg.wParam 
+    ret 
+WinMain4 endp
+
+
+WndProc4 proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM 
+    LOCAL hdc:HDC 
+    LOCAL ps:PAINTSTRUCT 
+    LOCAL rect:RECT 
+
+    .IF uMsg==WM_DESTROY 
+        invoke PostQuitMessage,0
+    .ELSEIF uMsg==WM_CREATE 
+    .ELSEIF uMsg == WM_KEYDOWN
+    .ELSEIF uMsg == WM_COMMAND
+        mov eax, wParam
+
+    .ELSEIF uMsg == WM_PAINT
+        invoke BeginPaint, hWnd, addr ps
+        mov hdc, eax
+        
+
+        ; 繪製平台
+        mov eax, cakeX
+        add eax, cakeWidth
+        mov edx, cakeY
+        add edx, cakeHeight
+        mov [tempWidth], eax
+        mov [tempHeight], edx
+        invoke Rectangle, hdc, cakeX, cakeY, tempWidth, tempHeight
+
+        invoke EndPaint, hWnd, addr ps
+    .ELSE 
+        invoke DefWindowProc,hWnd,uMsg,wParam,lParam 
+        ret
+    .ENDIF 
+    xor   eax, eax 
+    ret 
+WndProc4 endp 
+
+
+end
--- Test/main.asm
+++ Test/main.asm
@@ -6,11 +6,13 @@ EXTERN BreakOut@0: PROC
 EXTERN Advanced1A2B@0: PROC
 EXTERN Cake1@0: PROC
 EXTERN Home@0: PROC
+EXTERN Minesweeper@0: PROC
 Game1A2B EQU Game1A2B@0
 BreakOut EQU BreakOut@0
 Advanced1A2B EQU Advanced1A2B@0
 Cake1 EQU Cake1@0
 Home EQU Home@0
+Minesweeper EQU Minesweeper@0
 
 .data
 Option1 byte "1. 1A2B", 0
@@ -18,6 +20,7 @@ Option2 byte "2. BreakOut", 0
 Option3 byte "3. Cake1", 0
 Option4 byte "4. Advanced 1A2B", 0
 Option5 byte "5. Home", 0
+Option6 byte "6. Minesweeper", 0
 message7 byte "Please enter the game you choose:", 0
 message8 byte "Invalid Input!", 0
 
@@ -39,6 +42,9 @@ Start:
     call WriteString
     call Crlf
     mov edx, OFFSET Option5
+    call WriteString
+    call Crlf
+    mov edx, OFFSET Option6
     call WriteString
     call Crlf
     mov edx, OFFSET message7
@@ -71,8 +77,14 @@ Forth:
 
 Fifth:
     cmp eax, 5
-    jne Next
+    jne Sixth
     call Home  ; 呼叫 Game1A2B 程式
+    jmp Next
+
+Sixth:
+    cmp eax, 6
+    jne Next
+    call Minesweeper  ; 呼叫 Game1A2B 程式
     jmp Next
 
 InvalidInput:
