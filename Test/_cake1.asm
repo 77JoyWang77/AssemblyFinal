88908f95aea0b9df5bf5e90e8f6d858c43cec25c
77JoyWang77 <wangchaoyin77@gmail.com>
2024-12-06 02:49:16 +0800

    蛋糕剩緩衝和debug

--- Test/_cake1.asm
+++ Test/_cake1.asm
@@ -16,25 +16,32 @@ include kernel32.inc
 include gdi32.inc 
 
 WinMain3 proto :DWORD
+check_collision PROTO index:DWORD
 
 .DATA 
 ClassName db "SimpleWinClass", 0 
 AppName  db "Cake", 0 
 ButtonClassName db "button", 0 
-cakeX DWORD 100           ; 初始 X 座標
-cakeY DWORD 50           ; 初始 Y 座標
+cakeX DWORD 200           ; 初始 X 座標
+cakeY DWORD 80           ; 初始 Y 座標
 cakeWidth DWORD 50       ; 平台寬度
 cakeHeight DWORD 20       ; 平台高度
-stepSize DWORD 5              ; 每次移動的像素數量
+stepSize DWORD 50              ; 每次移動的像素數量
 winWidth DWORD 600              ; 視窗寬度
 winHeight DWORD 600             ; 視窗高度
 velocityX DWORD 5               ; 小球 X 方向速度
 velocityY DWORD 0               ; 小球 Y 方向速度
-border_right DWORD 550
-border_left DWORD 50
+border_right DWORD 450
+border_left DWORD 150
+maxCakes EQU 20                         ; 最大蛋糕數量
+currentCakeIndex DWORD 0                   ; 當前蛋糕索引
+cakes RECT maxCakes DUP(<0, 0, 0, 0>)      ; 蛋糕陣列，儲存每個蛋糕的邊界
+falling BOOL FALSE                         ; 是否有蛋糕正在掉落
+gameover BOOL FALSE
+fallSpeed DWORD 5                          ; 蛋糕掉落速度
 
-TriesRemaining  db 9
-RemainingTriesText db "Remaining:  ", 0
+TriesRemaining  byte 20
+RemainingTriesText db "Remaining:   ", 0
 EndGame db "Game Over!", 0
 line1Rect RECT <20, 20, 580, 40>
 
@@ -92,17 +99,16 @@ WinMain3 proc hInst:HINSTANCE
     ; 計算窗口寬度和高度
     mov eax, wr.right
     sub eax, wr.left
-    mov winWidth, eax
+    mov tempWidth, eax
 
     mov eax, wr.bottom
     sub eax, wr.top
-    mov winHeight, eax
+    mov tempHeight, eax
 
     ; 創建窗口
     invoke CreateWindowEx, NULL, ADDR ClassName, ADDR AppName, \
             WS_OVERLAPPED or WS_CAPTION or WS_SYSMENU or WS_MINIMIZEBOX, \
-            0, 0, winWidth, winHeight, \
-            NULL, NULL, hInst, NULL
+            0, 0, tempWidth, tempHeight, NULL, NULL, hInst, NULL
     mov   hwnd,eax 
     invoke SetTimer, hwnd, 1, 50, NULL  ; 更新間隔從 50ms 改為 10ms
     ; 顯示和更新窗口
@@ -127,14 +133,79 @@ WndProc3 proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
     LOCAL rect:RECT 
 
     .IF uMsg==WM_DESTROY 
-        invoke PostQuitMessage,NULL 
+        invoke PostQuitMessage,0
     .ELSEIF uMsg==WM_CREATE 
+        mov ebx, SIZEOF RECT
+        imul ebx, currentCakeIndex
+        mov eax, cakeX
+        mov cakes[ebx].left, eax
+        add eax, cakeWidth
+        mov cakes[ebx].right, eax
+        mov eax, cakeY
+        mov cakes[ebx].top, eax
+        add eax, cakeHeight
+        mov cakes[ebx].bottom, eax
 
     .ELSEIF uMsg == WM_TIMER
-        ; 更新小球位置
-        call update_cake
-        invoke InvalidateRect, hWnd, NULL, TRUE
+        invoke GetAsyncKeyState, VK_SPACE
+        test eax, 8000h ; 測試最高位
+        jz skip_space_key
 
+        ; 如果目前沒有正在掉落的蛋糕，啟動掉落邏輯
+        cmp falling, TRUE
+        je skip_space_key
+        mov falling, TRUE
+
+        ; 初始化新蛋糕位置
+        mov velocityX, 0
+        mov velocityY, 10
+
+    skip_space_key:
+        call update_cake
+        mov ebx, SIZEOF RECT
+        imul ebx, currentCakeIndex
+        mov eax, cakeX
+        mov cakes[ebx].left, eax
+        add eax, cakeWidth
+        mov cakes[ebx].right, eax
+        mov eax, cakeY
+        mov cakes[ebx].top, eax
+        add eax, cakeHeight
+        mov cakes[ebx].bottom, eax
+        
+        ; 檢查是否與其他蛋糕或地面接觸
+        cmp falling, TRUE
+        jne skip_fall
+        invoke check_collision, currentCakeIndex
+        cmp eax, TRUE
+        jne handle_collision
+
+        ; 如果沒有碰撞，重繪蛋糕
+        jmp skip_fall
+
+    handle_collision:
+        mov falling, FALSE
+        inc currentCakeIndex  ; 下一個蛋糕
+        dec TriesRemaining
+        mov cakeX, 200
+        mov cakeY, 80
+        mov velocityX, 5
+        mov velocityY, 0
+        cmp gameover, TRUE
+        je game_over
+        cmp currentCakeIndex, maxCakes
+        je game_over
+
+    skip_fall:
+        invoke InvalidateRect, hWnd, NULL, TRUE
+        ret
+    game_over:
+        ; 顯示遊戲結束訊息
+        invoke MessageBox, hWnd, addr EndGame, addr AppName, MB_OK
+        invoke DestroyWindow, hWnd
+        invoke PostQuitMessage, 0
+        ret
+    
     .ELSEIF uMsg == WM_COMMAND
         mov eax, wParam
 
@@ -147,19 +218,30 @@ WndProc3 proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
         mov hBrush, eax                          ; 存筆刷句柄
         invoke SelectObject, hdc, hBrush
 
+        mov bl, 10
         mov al, [TriesRemaining]       ; 將 TriesRemaining 的值載入 eax
+        div bl
+        mov byte ptr [RemainingTriesText + 11], ' '
+        cmp al, 0
+        je nextdigit
         add al, '0'                     ; 將數字轉換為 ASCII (單位數)
         mov byte ptr [RemainingTriesText + 11], al ; 將字元寫入字串
+        nextdigit:
+        add ah, '0'                     ; 將數字轉換為 ASCII (單位數)
+        mov byte ptr [RemainingTriesText + 12], ah ; 將字元寫入字串
         invoke DrawText, hdc, addr RemainingTriesText, -1, addr line1Rect,DT_CENTER
 
-        ; 繪製平台
-        mov eax, cakeX
-        add eax, cakeWidth
-        mov edx, cakeY
-        add edx, cakeHeight
-        mov [tempWidth], eax
-        mov [tempHeight], edx
-        invoke Rectangle, hdc, cakeX, cakeY, tempWidth, tempHeight
+        mov eax, currentCakeIndex
+    draw_cakes:
+        mov ebx, SIZEOF RECT
+        imul ebx
+        push eax
+        invoke Rectangle, hdc, cakes[eax].left, cakes[eax].top, cakes[eax].right, cakes[eax].bottom
+        pop eax
+        idiv ebx
+        dec eax
+        cmp eax, 0
+        jge draw_cakes
 
         invoke EndPaint, hWnd, addr ps
     .ELSE 
@@ -172,13 +254,11 @@ WndProc3 endp
 
 update_cake PROC
     ; 更新小球位置
+    cmp velocityX, 0
+    je movedown
     mov eax, cakeX
     add eax, velocityX
     mov cakeX, eax
-
-    mov eax, cakeY
-    add eax, velocityY
-    mov cakeY, eax
 
     ; 邊界碰撞檢測（鏡面反射）
     mov eax, cakeX
@@ -189,6 +269,10 @@ update_cake PROC
     cmp border_right, eax                ; 碰到右邊界
     jle reverse_x
 
+movedown:
+    mov eax, cakeY
+    add eax, velocityY
+    mov cakeY, eax
     jmp end_update                ; 若無碰撞，結束
 
 reverse_x:
@@ -198,4 +282,69 @@ end_update:
     ret
 update_cake ENDP
 
+check_collision PROC index:DWORD
+    LOCAL i:DWORD
+    LOCAL cr:RECT
+
+    mov eax, index
+    mov ebx, SIZEOF RECT              ; 獲取每個 RECT 結構的大小
+    imul ebx                   ; 計算 cakes[index] 的偏移量
+    mov ebx, cakes[eax].bottom
+    mov cr.bottom, ebx
+    mov ebx, cakes[eax].top
+    mov cr.top, ebx
+    mov ebx, cakes[eax].left
+    mov cr.left, ebx
+    mov ebx, cakes[eax].right
+    mov cr.right, ebx
+
+    ; 檢查是否碰到地面
+    mov ebx, cr.bottom
+    cmp ebx, winHeight
+    jge collision_found
+
+    cmp index, 0
+    je check_end
+check_other:
+    ; 檢查是否碰到其他蛋糕
+    mov ecx, index
+    dec ecx
+check_loop:
+    cmp ecx, 0
+    jl check_end
+
+    ; 比較左邊界和右邊界是否重疊
+    mov ebx, SIZEOF RECT
+    imul ebx, ecx
+check_left:
+    mov eax, cakes[ebx].left
+    cmp cr.right, eax
+    jle next_check
+    
+check_right:
+    mov eax, cakes[ebx].right
+    cmp cr.left, eax
+    jge next_check
+
+check_bottom:
+    mov eax, cakes[ebx].top
+    cmp cr.bottom, eax
+    jge collision_found
+
+next_check:
+    dec ecx
+    jmp check_loop
+
+check_end:
+    mov eax, TRUE
+    ret
+
+collision_found:
+    cmp currentCakeIndex, 0
+    je game_not_over
+    mov gameover, TRUE
+game_not_over:
+    mov eax, FALSE
+    ret
+check_collision ENDP
 end
